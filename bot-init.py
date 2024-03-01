cfb81c17ea6a3eb73d72f25090e8533dffa04fc7
Abdist <nasiefabdullah@gmail.com>
2024-03-01 14:05:54 +0100

    huh

--- bot-init.py
+++ bot-init.py
@@ -35,23 +35,10 @@ async def check_bot_status():
             if music_cog:
                 music_cog.clear()
             await vc.disconnect()
-            
-
-# i am using this to let me know wheter the bot is playing music with an onboard led
-@tasks.loop(seconds=2)
-async def check_playing_status():
-    for vc in bot.voice_clients:
-        if vc.is_playing():
-            process = subprocess.Popen('echo 1 > /sys/class/leds/working/brightness', stdout=subprocess.PIPE, shell=True)
-        else:
-            process = subprocess.Popen('echo 0 > /sys/class/leds/working/brightness', stdout=subprocess.PIPE, shell=True)
 
 # Event handler for bot ready
 @bot.event
 async def on_ready():
-# reseting the leds
-    process = subprocess.Popen('echo 0 > /sys/class/leds/auxiliary/brightness', stdout=subprocess.PIPE, shell=True)
-    process = subprocess.Popen('echo 0 > /sys/class/leds/working/brightness', stdout=subprocess.PIPE, shell=True)
     print_text(Ori(), TextColor.CYAN)
     print_text(f'{bot.user.name}', TextColor.PURPLE) 
     print(' is ready! ^^')
@@ -59,11 +46,6 @@ async def on_ready():
     global check_bot_status
     if not check_bot_status.is_running():
         check_bot_status.start()
-    global check_playing_status
-    if not check_playing_status.is_running():
-        check_playing_status.start()
 
 bot.load_extension('cogs.music')
-bot.load_extension('cogs.wichteln')
-bot.load_extension('cogs.minecraft')
 bot.run(discord_token)
